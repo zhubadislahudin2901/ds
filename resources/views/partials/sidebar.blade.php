3333553a0e9c9def8d23be72189dd2742c0df512
ulum <apumm12@gmail.com>
2025-05-26 23:56:42 +0700

    done all

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -26,10 +26,14 @@
                 <i class="bi bi-newspaper me-2"></i> Berita
             </a>
         </li>
-        <li class="nav-item mt-4">
-            <a class="nav-link text-danger" href="/logout">
-                <i class="bi bi-box-arrow-right me-2"></i> Logout
-            </a>
-        </li>
+       <li class="nav-item mb-2 mt-4">
+    <form id="logout-form" action="{{ route('logout') }}" method="POST">
+        @csrf
+        <button type="submit" class="nav-link text-danger bg-transparent border-0 p-0 m-0 d-flex align-items-center" style="cursor: pointer;">
+            <i class="bi bi-box-arrow-right me-2"></i> Logout
+        </button>
+    </form>
+</li>
+
     </ul>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,15 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BeritaController;
+use App\Http\Controllers\GuruController;
+use App\Http\Controllers\VisiMisiController;
+use App\Http\Controllers\FasilitasController;
 
-// Home
+//Home (Frontend)
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/home', [HomeController::class, 'index']);
 Route::get('/fasilitas', [HomeController::class, 'fasilitas']);
@@ -16,27 +19,32 @@ Route::get('/akreditasi', [HomeController::class, 'akreditasi']);
 Route::get('/kontak', [HomeController::class, 'kontak']);
 Route::get('/berita', [HomeController::class, 'berita']);
 
-// Auth
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/login', [AuthController::class, 'authenticated']);
-Route::get('/logout', [AuthController::class, 'logout']);
+//Authentication
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'authenticated']);
+});
 
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
 
-// Dashboard
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+    //Dashboard
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-// berita
-Route::get('/admin/berita/create', [BeritaController::class, 'create'])->name('berita.create');
-Route::post('/admin/berita/store', [BeritaController::class, 'store'])->name('berita.store');
-Route::get('/admin/berita', [BeritaController::class, 'index'])->name('berita.index');
-Route::get('/admin/berita/{id}/edit', [BeritaController::class, 'edit'])->name('berita.edit');
-Route::put('/admin/berita/{id}', [BeritaController::class, 'update'])->name('berita.update');
-Route::delete('/admin/berita/{id}', [BeritaController::class, 'destroy'])->name('berita.destroy');
+    //Admin: Berita
+    Route::get('/admin/berita', [BeritaController::class, 'index'])->name('berita.index');
+    Route::get('/admin/berita/create', [BeritaController::class, 'create'])->name('berita.create');
+    Route::post('/admin/berita/store', [BeritaController::class, 'store'])->name('berita.store');
+    Route::get('/admin/berita/{id}/edit', [BeritaController::class, 'edit'])->name('berita.edit');
+    Route::put('/admin/berita/{id}', [BeritaController::class, 'update'])->name('berita.update');
+    Route::delete('/admin/berita/{id}', [BeritaController::class, 'destroy'])->name('berita.destroy');
 
-//guru
-Route::resource('/admin/guru', App\Http\Controllers\GuruController::class);
+    //Admin: Guru
+    Route::resource('/admin/guru', GuruController::class);
 
-// visimisi
-Route::resource('/admin/visi-misi', App\Http\Controllers\VisiMisiController::class);
+    //Admin: Visi Misi
+    Route::resource('/admin/visi-misi', VisiMisiController::class);
 
-Route::resource('/admin/fasilitas', App\Http\Controllers\FasilitasController::class);
+    //Admin: Fasilitas
+    Route::resource('/admin/fasilitas', FasilitasController::class);
+});
