24520dac823d2bd9f9ef848a72bc62ffea3bc83f
zhubadislahudin2901 <115449979+zhubadislahudin2901@users.noreply.github.com>
2025-05-27 23:44:04 +0700

    home view

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,32 +1,32 @@
-    <header id="header" class="header d-flex align-items-center fixed-top">
-        <div class="container-fluid container-xl position-relative d-flex align-items-center">
+<header id="header" class="header d-flex align-items-center fixed-top">
+    <div class="container-fluid container-xl position-relative d-flex align-items-center">
 
-            <a href="index.html" class="logo d-flex align-items-center me-auto">
-                <!-- Uncomment the line below if you also wish to use an image logo -->
-                <!-- <img src="assets/img/logo.png" alt=""> -->
-                <h1 class="sitename">SDN 01 DOROREJO</h1>
-            </a>
+        <a href="index.html" class="logo d-flex align-items-center me-auto">
+            <!-- Uncomment the line below if you also wish to use an image logo -->
+            <!-- <img src="assets/img/logo.png" alt=""> -->
+            <h1 class="sitename">SDN 01 DOROREJO</h1>
+        </a>
 
-            <nav id="navmenu" class="navmenu">
-                <ul>
-                    <li><a href="home" class="active">Beranda</a></li>
-                    <!-- Dropdown Menu -->
-                    <li class="dropdown">
-                        <a href="#"><span>Profil</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-                        <ul>
-                            <li><a href="fasilitas">Fasilitas Sekolah</a></li>
-                            <li><a href="visimisi">Visi Misi</a></li>
-                            <li><a href="dataguru">Data Guru</a></li>
-                        </ul>
-                    </li>
+        <nav id="navmenu" class="navmenu">
+            <ul>
+                <li><a href="home" class="active">Beranda</a></li>
+                <!-- Dropdown Menu -->
+                <li class="dropdown">
+                    <a href="#"><span>Profil</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
+                    <ul>
+                        <li><a href="fasilitas">Fasilitas Sekolah</a></li>
+                        <li><a href="visimisi">Visi Misi</a></li>
+                        <li><a href="dataguru">Data Guru</a></li>
+                    </ul>
+                </li>
 
-                    <li><a href="akreditasi">Akreditasi</a></li>
-                    <li><a href="berita">Berita</a></li>
-                    <li><a href="kontak">Kontak</a></li>
-                </ul>
-                <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
-            </nav>
+                <li><a href="akreditasi">Akreditasi</a></li>
+                <li><a href="berita">Berita</a></li>
+                <li><a href="kontak">Kontak</a></li>
+            </ul>
+            <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+        </nav>
 
 
-        </div>
-    </header>
+    </div>
+</header>
