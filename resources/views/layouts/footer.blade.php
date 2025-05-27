5bced1dcfd3cd9f4efd82ced0ee9ef386188028b
ulum <apumm12@gmail.com>
2025-05-27 21:54:18 +0700

    layout nav and foot

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,42 @@
+
+    <footer id="footer" class="footer dark-background">
+        <div class="footer-custom d-flex flex-wrap justify-content-center text-start">
+            <div class="info-box">
+                <i class="bi bi-telephone"></i>
+                <div class="info-text">
+                    <small>Kontak</small>
+                    <a href="tel:(024)7606340">(024) 7606340</a>
+                </div>
+            </div>
+            <div class="info-box">
+                <i class="bi bi-envelope"></i>
+                <div class="info-text">
+                    <small>Email Kami</small>
+                    <a href="mailto:sdn01dororejo@yahoo.com">sdn01dororejo@yahoo.com</a>
+                </div>
+            </div>
+            <div class="info-box">
+                <i class="bi bi-geo-alt"></i>
+                <div class="info-text">
+                    <small>Alamat</small>
+                    XMCP+836, Sawah Dan Kebun, Dororejo, Kec. Doro, Kabupaten Pekalongan, Jawa Tengah 51191
+                </div>
+            </div>
+        </div>
+
+        <!-- Bagian Media Sosial -->
+        <div class="social-media d-flex justify-content-center mt-4">
+            <a href="https://facebook.com/" target="_blank" class="me-3">
+                <i class="bi bi-facebook" style="font-size: 24px; color: #fff;"></i>
+            </a>
+            <a href="https://instagram.com/" target="_blank" class="me-3">
+                <i class="bi bi-instagram" style="font-size: 24px; color: #fff;"></i>
+            </a>
+            <a href="https://youtube.com/" target="_blank">
+                <i class="bi bi-youtube" style="font-size: 24px; color: #fff;"></i>
+            </a>
+        </div>
+        <div class="container copyright text-center mt-4">
+            <p>©<strong class="px-1 sitename">SDN 01 DOROREJO</strong></p>
+        </div>
+    </footer>
---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <title>SDN 01 DOROREJO</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+
+    <!-- Favicons -->
+    <link href="assets/img/favicon.png" rel="icon">
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com" rel="preconnect">
+    <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&family=Raleway:wght@100;200;300;400;500;600;700;800;900&family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Main CSS File -->
+    <link href="assets/css/main.css" rel="stylesheet">
+</head>
+       <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/vendor/aos/aos.js"></script>
+        <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+        <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+        <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+        <script src="assets/vendor/php-email-form/validate.js"></script>
+
+        <!-- Main JS File -->
+        <script src="assets/js/main.js"></script>
+        <body>
+              @include('layouts.navbar')
+
+    <div class="main-content">
+        @yield('content')
+    </div>
+@include('layouts.footer')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+        AOS.init();
+    </script>
+        </body>
+</html>
