cc9ae1339d58f2385cdf89debcaf1eb9595cb01c
ulum <apumm12@gmail.com>
2025-05-31 22:10:25 +0700

    change navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,32 +1,37 @@
 <header id="header" class="header d-flex align-items-center fixed-top">
-    <div class="container-fluid container-xl position-relative d-flex align-items-center">
+  <div class="container-fluid container-xl position-relative d-flex align-items-center">
 
-        <a href="index.html" class="logo d-flex align-items-center me-auto">
-            <!-- Uncomment the line below if you also wish to use an image logo -->
-            <!-- <img src="assets/img/logo.png" alt=""> -->
-            <h1 class="sitename">SDN 01 DOROREJO</h1>
-        </a>
+    <a href="{{ url('/') }}" class="logo d-flex align-items-center me-auto">
+      <h1 class="sitename">SDN 01 DOROREJO</h1>
+    </a>
 
-        <nav id="navmenu" class="navmenu">
-            <ul>
-                <li><a href="home" class="active">Beranda</a></li>
-                <!-- Dropdown Menu -->
-                <li class="dropdown">
-                    <a href="#"><span>Profil</span> <i class="bi bi-chevron-down dropdown-indicator"></i></a>
-                    <ul>
-                        <li><a href="fasilitas">Fasilitas Sekolah</a></li>
-                        <li><a href="visimisi">Visi Misi</a></li>
-                        <li><a href="dataguru">Data Guru</a></li>
-                    </ul>
-                </li>
+    <nav id="navmenu" class="navmenu">
+<ul>
+  <li>
+    <a href="{{ url('/home') }}" class="{{ Request::is('home') || Request::is('/') ? 'active' : '' }}">Beranda</a>
+  </li>
 
-                <li><a href="akreditasi">Akreditasi</a></li>
-                <li><a href="berita">Berita</a></li>
-                <li><a href="kontak">Kontak</a></li>
-            </ul>
-            <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
-        </nav>
+  <!-- Dropdown Menu -->
+  <li class="dropdown {{ Request::is('fasilitas') || Request::is('visimisi') || Request::is('dataguru') ? 'active' : '' }}">
+    <a href="#" class="{{ Request::is('fasilitas') || Request::is('visimisi') || Request::is('dataguru') ? 'active' : '' }}">
+      <span>Profil</span> <i class="bi bi-chevron-down dropdown-indicator"></i>
+    </a>
+    <ul>
+      <li><a href="{{ url('/fasilitas') }}" class="{{ Request::is('fasilitas') ? 'active' : '' }}">Fasilitas Sekolah</a></li>
+      <li><a href="{{ url('/visimisi') }}" class="{{ Request::is('visimisi') ? 'active' : '' }}">Visi Misi</a></li>
+      <li><a href="{{ url('/dataguru') }}" class="{{ Request::is('dataguru') ? 'active' : '' }}">Data Guru</a></li>
+    </ul>
+  </li>
 
+<li>
+  <a href="{{ route('akreditasi') }}" class="{{ request()->routeIs('akreditasi') ? 'active' : '' }}">Akreditasi</a>
+</li>
 
-    </div>
+  <li><a href="{{ url('/berita') }}" class="{{ Request::is('berita') ? 'active' : '' }}">Berita</a></li>
+  <li><a href="{{ url('/kontak') }}" class="{{ Request::is('kontak') ? 'active' : '' }}">Kontak</a></li>
+</ul>
+
+      <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+    </nav>
+  </div>
 </header>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/home', [HomeController::class, 'index']);
 Route::get('/fasilitas', [HomeController::class, 'fasilitas']);
 Route::get('/visimisi', [HomeController::class, 'visimisi']);
 Route::get('/dataguru', [HomeController::class, 'dataguru']);
-Route::get('/akreditasi', [HomeController::class, 'akreditasi']);
+Route::get('/akreditasi', [HomeController::class, 'akreditasi'])->name('akreditasi');
 Route::get('/kontak', [HomeController::class, 'kontak']);
 Route::get('/berita', [HomeController::class, 'berita']);
 
