7dcd3798a5b0959c93805e4a6635f875e4c07b5f
zhubadislahudin2901 <115449979+zhubadislahudin2901@users.noreply.github.com>
2025-07-06 22:33:06 +0700

    users

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class RoleMiddleware
+{
+    public function handle(Request $request, Closure $next, $role)
+    {
+        if (auth()->check() && auth()->user()->role === $role) {
+            return $next($request);
+        }
+
+        abort(403, 'Unauthorized');
+    }
+}
